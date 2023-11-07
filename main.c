7f604b0b8604dc3a718524f112b4f38731cc8243
sjw000324@gmail.com <sjw000324@gmail.com>
2023-11-07 20:08:57 +0900

    basic commit

--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("아 과제 하기싫다");
-    return 0;
-}
---
+++ source/ScreenRender.h
@@ -0,0 +1,8 @@
+#ifndef _SCREENRENDER_
+#define _SCREENRENDER_
+
+#include <stdbool.h>
+#include <string.h>
+#include <windows.h>
+
+#endif
