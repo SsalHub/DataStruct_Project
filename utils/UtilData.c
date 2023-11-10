9d867da33709f099a24c51de5f48a3d6a542fb77
sjw000324@gmail.com <sjw000324@gmail.com>
2023-11-10 23:09:57 +0900

    feat : add waitForSeconds()

--- utils/UtilData.c
+++ utils/UtilData.c
@@ -5,3 +5,12 @@ void gotoxy(int x, int y)
     printf("\033[%d;%df",y,x);
 	fflush(stdout);
 }
+
+void waitForSeconds(float s)
+{
+    #ifdef _WIN32 || _WIN64
+    Sleep(s * 1000);    // windows.h
+    #else
+    sleep(s);           // unistd.h
+    #endif
+}
--- utils/UtilData.h
+++ utils/UtilData.h
@@ -2,7 +2,13 @@
 #define __UTILDATA__
 
 #include <stdio.h>
+#ifdef _WIN32 || _WIN64
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 
 void gotoxy(int x, int y);
+void waitForSeconds(float s);
 
 #endif
