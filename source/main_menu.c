fa5e605bef47b1d68e8c6daf4f720cf444fd39cd
ddinggul <130947598+ddinggul@users.noreply.github.com>
2023-11-21 22:30:08 +0900

    main함수에서 준서용 설정 (주석 삭제)

--- source/main_menu.c
+++ source/main_menu.c
@@ -6,8 +6,10 @@ void showMainMenu()
     char buffer[64];
     int x, y, len;
 
+
     strcpy(buffer, "Main Menu");
     printString(buffer, _ALIGN_CENTER_, _ALIGN_CENTER_);
+
 
     strcpy(buffer, "Press Any Key.");
     printString(buffer, _ALIGN_CENTER_, _ALIGN_BOTTOM_);
