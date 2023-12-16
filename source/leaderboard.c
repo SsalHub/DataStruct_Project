c72dcd38d943a3a84a449a98448d2fac6d4987d6
jjjcastle <wnstjd0606@gmail.com>
2023-12-16 20:17:06 +0900

    파일 경로 변경이후 출력이 안됨(해결 x)

--- source/leaderboard.c
+++ source/leaderboard.c
@@ -1,11 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "leaderboard.h"
-#define MAX_NAME_LENGTH 20
-
-typedef struct {
-    int score;
-    char name[MAX_NAME_LENGTH];
-} Entry;
 
 void bubbleSort(Entry arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
@@ -36,7 +30,7 @@ void drawScoreboard() {
     printString("|          점수판        |", 19, 2);
     printString("+------------------------+", 19, 3);
 
-    const char* fp = "C:\\Users\\user7\\OneDrive\\바탕 화면\\Data_struct_sub\\data_structproject\\DataStruct_Project\\resources\\ranking.dat";
+    const char* fp = RANKING_PATH;
     FILE* file = fopen(fp, "r");
 
     if (file == NULL) {
@@ -74,7 +68,7 @@ void drawScoreboard() {
 void updateScoreboard(int newScore, const char* newName) {
     Entry entries[10];
     char filter[50];
-    FILE* file = fopen("C:\\Users\\user7\\OneDrive\\바탕 화면\\Data_struct_sub\\data_structproject\\DataStruct_Project\\resources\\ranking.dat", "r");
+    FILE* file = fopen(RANKING_PATH,"r");
 
     if (file == NULL) {
         printString("파일 열기 실패\n", 19, 1);
@@ -101,13 +95,13 @@ void updateScoreboard(int newScore, const char* newName) {
     if (newScore > entries[9].score) {
         entries[9].score = newScore;
         strncpy(entries[9].name, newName, MAX_NAME_LENGTH - 1);
-        entries[9].name[MAX_NAME_LENGTH - 1] = '\0'; // Ensure null-termination
+        entries[9].name[MAX_NAME_LENGTH - 1] = '\0'; 
 
         
         bubbleSort(entries, 10);
 
         
-        file = fopen("C:\\Users\\user7\\OneDrive\\바탕 화면\\Data_struct_sub\\data_structproject\\DataStruct_Project\\resources\\ranking.dat", "w");
+        file = fopen(RANKING_PATH, "w");
         if (file == NULL) {
             printString("파일 쓰기 실패\n", 19, 1);
             exit(1);
--- source/leaderboard.h
+++ source/leaderboard.h
@@ -8,6 +8,12 @@
 #include <windows.h>
 
 #define RANKING_PATH "..\\resources\\ranking.dat"
+#define MAX_NAME_LENGTH 20
+
+typedef struct {
+    int score;
+    char name[MAX_NAME_LENGTH];
+} Entry;
 
 void drawScoreboard();
 
