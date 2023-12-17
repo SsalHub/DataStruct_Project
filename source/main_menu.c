c9e83d43cbd1bfdfa04beed44fd9587ec2c7983d
ddinggul <130947598+ddinggul@users.noreply.github.com>
2023-12-17 16:21:32 +0900

    메뉴 출력 추가

--- source/main_menu.c
+++ source/main_menu.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "main_menu.h"
 
+
 void showMainMenu()
 {
     char buffer[64];
@@ -21,6 +22,44 @@ void showMainMenu()
     }
     
     fclose(file);
-    while (!_kbhit());
 
+
+    // 메뉴 출력
+    gotoxy(20, 16);
+    fprintf(stdout, "1. Start Game");
+
+    gotoxy(20, 18);
+    fprintf(stdout, "2. Option");
+
+    gotoxy(20, 20);
+    fprintf(stdout, "3. Ranking");
+
+    gotoxy(20, 22);
+    fprintf(stdout, "4. Exit Game");
+
+    // 사용자 입력 받기
+    char choice = _getch();
+
+    // 사용자의 선택에 따라 동작
+    switch (choice) {
+    case '1':
+        // Game start
+        break;
+    case '2':
+        // Option
+        ShowSettings();
+        break;
+    case '3':
+        // Ranking
+        drawScoreboard();
+        break;
+    case '4':
+        // exit
+        system("cls");
+        break;
+    default:
+        fprintf(stdout, "잘못 입력하였습니다.");
+        break;
+    }
+    while (!_kbhit());
 }
--- source/main_menu.h
+++ source/main_menu.h
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <windows.h>
 
-#define LOGO_PATH	"..\\resources\\title_logo.txt"
+#define LOGO_PATH "C:\\Users\\junseo\\Desktop\\datastruct\\teamproject\\datastruct_project\\DataStruct_Project\\resources\\title_logo.txt"
 
 void showMainMenu();
 
